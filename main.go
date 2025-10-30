6bf58e6c3b122fe1062947ea531a8f3dac40c96d
justinmnge <justin.mnge@gmail.com>
2025-10-29 20:34:10 -0600

    feat ✨: Created simple web page with only Go and HTML

--- main.go
+++ main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handlerRoot)
+	mux.HandleFunc("/", handleHome)
+
+	fmt.Println("Server listening to :8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func handlerRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	t, err := template.ParseFiles("static/" + tmpl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	t.Execute(w, nil)
 }
